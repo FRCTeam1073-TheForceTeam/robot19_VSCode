8aad0e0b19b7074fba2b9c06f035cdeb8d5673f3
bugeybot <bugeybot@gmail.com>
2019-01-06 12:42:31 -0500

    Initial Base Commit
    Signed-off-by: bugeybot <bugeybot@gmail.com>

---
+++ src/main/java/frc/robot/OI.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+	
+    public OI() {
+    }
+}
+
+
