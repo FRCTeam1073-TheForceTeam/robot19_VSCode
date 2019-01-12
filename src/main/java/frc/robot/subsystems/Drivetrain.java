a70ce3d29cc5424b985b51ee960637567f57014a
122004 <ashtree012@gmail.com>
2019-01-12 08:37:34 -0600

    Another Update
    Fixed a few things.

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,15 +1,18 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
 /**Author: Alex Hill, tons of help from Nathaniel
 * Not tested yet, will do so next meeting. :)
 */
 
 // imports reference folders
-import frc.robot.*;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.RobotMap;
+import frc.robot.commands.ExampleCommand;
 
 //Identifying and assigning motors, also putting methods to control the system
 
@@ -20,15 +23,14 @@ public class Drivetrain extends Subsystem {
     private final WPI_TalonSRX leftMotor1 = RobotMap.leftMotor1;
     private final WPI_VictorSPX leftMotor2 = RobotMap.leftMotor2;
 
-
     public DifferentialDrive difDrive;
 
-	public Drivetrain() {
+    public Drivetrain() {
         rightMotor1.setInverted(false);
         rightMotor2.setInverted(false);
-		leftMotor1.setInverted(false);
-		leftMotor2.setInverted(false);
-        
+        leftMotor1.setInverted(false);
+        leftMotor2.setInverted(false);
+
         rightMotor2.follow(rightMotor1);
         leftMotor2.follow(leftMotor1);
 
@@ -37,16 +39,16 @@ public class Drivetrain extends Subsystem {
         leftMotor1.setSafetyEnabled(false);
         leftMotor2.setSafetyEnabled(false);
 
-        difDrive = new DifferentialDrive(RobotMap.rightMotor1, Robotmap.leftMotor1);
+        difDrive = new DifferentialDrive(RobotMap.rightMotor1, RobotMap.leftMotor1);
 
-	}   
+    }
 
-//Setting deadzone as default on the controller
+    @Override
+    // Setting deadzone as default on the controller
+    public void initDefaultCommand() {
+        setDefaultCommand(new ExampleCommand());
 
-	public void initDefaultCommand(){
-        setDefaultCommand(new DriveControls(Presets.deadzone));
-
-	}
+    }
 
     public void periodic() {
 
