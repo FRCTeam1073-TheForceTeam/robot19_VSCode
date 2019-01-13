71a1d0781a654ac6733f323a7f1d4d7493c00b44
bugeybot <bugeybot@gmail.com>
2019-01-13 11:34:42 -0500

    Clearing up unused stuff

--- src/main/java/frc/robot/subsystems/GearBox.java
+++ src/main/java/frc/robot/subsystems/GearBox.java
@@ -8,7 +8,6 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-import frc.robot.OI;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
 
--- src/main/java/frc/robot/subsystems/Pnuematic.java
+++ src/main/java/frc/robot/subsystems/Pnuematic.java
@@ -6,8 +6,7 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
- 
-import edu.wpi.first.wpilibj.Compressor;
+
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
@@ -20,7 +19,6 @@ public class Pnuematic extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
   
-  private final Compressor compressor = RobotMap.compressor;
 	private final Solenoid high = RobotMap.high;
   private final Solenoid low = RobotMap.low;
   
