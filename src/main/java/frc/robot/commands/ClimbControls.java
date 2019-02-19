a18e8fdff9d49abeafa690a3d2aea913266223a4
hallettc <iprogram20@gmail.com>
2019-02-18 20:07:42 -0500

    Hatch and Climber
    Adds the hatch collector and the climber
    -Untested

---
+++ src/main/java/frc/robot/commands/ClimbControls.java
@@ -0,0 +1,105 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+/**
+ * This is the climber movement controls
+ * for the teleoperated period of a match.
+ * It is also the default command for 
+ * the Climber.java subsystem.
+ * 
+ * This command does not finish.
+ * 
+ * @author Cam
+ * @see /subsystems/Climber.java
+ * @category Climb Command
+ */
+public class ClimbControls extends Command {
+	
+	/** Controller Dead Zone */
+	private double deadzone;
+
+	/** Controller Data: Left Y */
+	private double forward;
+	
+	/** Controller Data: Right X */
+	private double rotational;
+
+	/** Output for Motor Power */
+	private double leftClimbOutput, rightClimbOutput;
+
+	/** Just a delay */
+	private double executes = 0;
+
+
+	/**
+	 * This is the climber movement controls
+	 * for the teleoperated period of a match.
+	 * It is also the default command for
+	 * the climber.java subsystem.
+	 * 
+	 * This command requires the drivetrain subsystem
+	 * so as to give it priority over other commands 
+	 * accessing the drivetrain.
+	 * 
+	 * This command does not finish.
+	 * 
+	 * @author Cam
+	 * @see /subsystems/Climber.java
+	 * @category Drive Command
+	 */
+	public ClimbControls(double deadzone) {
+		requires(Robot.drivetrain);
+		this.deadzone = deadzone;
+	}
+
+	/** Called Repeatedly */
+	protected void execute() {
+		/* Controller Data */
+		//right bumper
+		forward = Robot.oi.operatorControl.getRawAxis(3);
+		
+		/* Outputs Checked Controller Data to Motors */
+		tankClimb(deadZoneCheck(forward));
+	}
+
+	/**
+   	 * Tank climb method for differential drive platform.
+   	 *
+   	 * @param fwd The climber's speed along the X axis [-1.0..1.0]. Forward is positive.
+   	 */
+	public void tankClimb(double fwd) {
+		
+		Robot.drivetrain.tank(limit(fwd), limit(fwd));
+
+	}
+
+	/** 
+	 * @param val Input to check against dead zone
+	 * @return If within dead zone return 0, Else return val
+	 */
+	private double deadZoneCheck(double val) {
+		if (Math.abs(val) < deadzone) return 0;
+		return val;
+
+	}
+
+  	/**
+   	 * Limit motor values to the -1.0 to +1.0 range.
+   	 */
+  	private double limit(double value) {
+    	if (Math.abs(value) > 1.0) return Math.copySign(1, value);
+    	return value;
+  	}
+
+	/** 
+	 * This command should never finish as it 
+	 * must remain active for the duration of
+	 * any teleoperated period, and is only run
+	 * during the teleoperated period.
+	 */
+	protected boolean isFinished() {
+		return false;
+	}
+}
--- src/main/java/frc/robot/subsystems/Hatch.java
+++ src/main/java/frc/robot/subsystems/Hatch.java
@@ -1,30 +1,77 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.Presets;
+import frc.robot.Robot;
 import frc.robot.RobotMap;
+import frc.robot.commands.DriveControls;
+import frc.robot.commands.*;
 
-/**
- * An example subsystem.  You should replace me with your own Subsystem.
- */
 public class Hatch extends Subsystem {
-
-  public final WPI_TalonSRX hatchCollect = RobotMap.hatchCollect;
+    
 	public final WPI_TalonSRX hatchLift = RobotMap.hatchLift;
+	public final WPI_TalonSRX hatchCollect = RobotMap.hatchCollect;
+	private double lift;
+	private double collect;
+	
+	public Hatch() {
+		/* Reset all motors */
+		hatchLift.configFactoryDefault();
+		hatchCollect.configFactoryDefault();
 
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
+    	hatchLift.setSafetyEnabled(false);
+		hatchCollect.setSafetyEnabled(false);
+		/* Set Neutral Mode */
+		hatchLift.setNeutralMode(NeutralMode.Brake);
+		hatchCollect.setNeutralMode(NeutralMode.Brake);
+		
+		/* Configure the left Talon's selected sensor to a Quad Encoder*/
+		hatchLift.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, Presets.timeoutMS);
+		
+		/* Configure the right Talon's selected sensor to a Quad Encoder*/
+		hatchCollect.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, Presets.timeoutMS);
+		
+		/* Configure output and sensor direction */
+		hatchLift.setInverted(false);
+		hatchCollect.setInverted(false);
 
-  @Override
-  public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
-  }
+
+		/**
+		 * Max out the peak output (for all modes).  
+		 * However you can limit the output of a given PID object with configClosedLoopPeakOutput().
+		 */
+		hatchLift.configPeakOutputForward(1.0, Presets.timeoutMS);
+		hatchCollect.configPeakOutputReverse(-1.0, Presets.timeoutMS);
+		
+
+	}
+    
+    @Override
+    public void initDefaultCommand() {
+    	setDefaultCommand(new HatchControls(Presets.deadzone));
+	}
+
+    public void periodic() {
+		lift = hatchLift.getSelectedSensorPosition();
+		collect = hatchCollect.getSelectedSensorPosition();
+
+		/*if (Robot.networktable.table.getEntry("changeP").getDouble(P)  != P ||
+		Robot.networktable.table.getEntry("changeI").getDouble(I) != I ||
+		Robot.networktable.table.getEntry("changeD").getDouble(D) != D ||
+		Robot.networktable.table.getEntry("changeK").getDouble(K) != K ||
+		(int)Robot.networktable.table.getEntry("changeIZ").getDouble(IZ) != IZ ||
+		Robot.networktable.table.getEntry("changePO").getDouble(PO) != PO ||
+		(int)Robot.networktable.table.getEntry("changeCLE").getDouble(CLE) != CLE) PIDChange();*/
+	}
+
+	public void liftCollect(double lift, double collect) {
+		hatchLift.set(ControlMode.PercentOutput, lift);
+		hatchCollect.set(ControlMode.PercentOutput, collect);
+	}
+
 }
