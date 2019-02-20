388d4eb1c87049d83137d9fa1fc86ada0b5ef751
Jack M <jackmcevoy15@gmail.com>
2019-02-19 20:18:40 -0500

    Also Another Thing Don't Worry
    Woo
    I'm kinda done with detailed patch notes

--- src/main/java/frc/robot/commands/HatchControls.java
+++ src/main/java/frc/robot/commands/HatchControls.java
@@ -9,7 +9,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
-public class HatchControls extends Command {
 
 /**
  * This is the climber movement controls
