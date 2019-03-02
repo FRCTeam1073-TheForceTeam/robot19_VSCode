0abd7f1c33180b82935dab5e738a6568985dc36b
122004 <ashtree012@gmail.com>
2019-03-02 09:26:06 -0600

    Revert "Setting Up Hatch Mechanisms (Nathaniel add in what you please)"
    This reverts commit 1849de695516ebc87851509aad387359b90b8716.

--- src/main/java/frc/robot/commands/HatchControls.java
+++ src/main/java/frc/robot/commands/HatchControls.java
@@ -39,6 +39,7 @@ public class HatchControls extends Command {
 	/** Just a delay */
 	private double executes = 0;
 
+
 	/**
 	 * This is the climber movement controls
 	 * for the teleoperated period of a match.
