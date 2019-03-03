61d6d9aa9d41639d8aa8a8c6027129537e3bc702
hallettc <iprogram20@gmail.com>
2019-03-03 16:33:24 -0500

    Fixes the waypoint issue
    Co-Authored-By: Alex <122004@users.noreply.github.com>

--- src/main/java/frc/robot/LiDAR/LidarDrive.java
+++ src/main/java/frc/robot/LiDAR/LidarDrive.java
@@ -8,45 +8,50 @@
 package frc.robot.LiDAR;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
+import frc.robot.RobotMap;
 import frc.robot.LiDAR.*;
 
 /**
  * Controls for LiDAR Localization
  * @author AlexHill and CamH
  */
-public class LidarDrive {
+public class LidarDrive extends Command {
     public static final double initialLatitude = Robot.networktable.table.getEntry("XCoord").getDouble(0);
 	public static final double initialLongitude = Robot.networktable.table.getEntry("YCoord").getDouble(0);
 	public static final double speed = Robot.networktable.table.getEntry("robotVelocity").getDouble(0); // ~0.05m - 0.1m per step
 	public static final double radius = 18401.945/2;  // 9200.9725 mm
-
+    private String name = SmartDashboard.getString("Waypoint Name", "test");
+	private double xCoord = SmartDashboard.getNumber("X", 0);
+    private double yCoord = SmartDashboard.getNumber("Y", 0)
+    private double initGyro;
 	public Location currentLocation = new Location("currentLocation", initialLatitude, initialLongitude);
-    
     public int waypointCounter = 0;
-	
-    public Route simulatedRoute = new Route(
-    		new Location("Waypoint 1", 48.137413, 11.561020),
-			new Location("Waypoint 2", 48.137370, 11.564539),
-			new Location("Waypoint 3", 48.137449, 11.565000),
-			new Location("Waypoint 4", 48.137578, 11.565311));
+    public Route destination;
+
     
-	public void move(){
-    	Location nextWaypoint = simulatedRoute.waypoints[waypointCounter];
-    	if (GeoAssist.calcDistance(currentLocation, nextWaypoint) < radius) {
-    		waypointCounter++;
-    		if (waypointCounter > simulatedRoute.waypoints.length-1) {
-    			currentLocation = new Location("currentLocation", initialLatitude, initialLongitude);
-    			waypointCounter = 0;
-    		}
-    		nextWaypoint = simulatedRoute.waypoints[waypointCounter];
-    	}
-    	System.out.println("Moving to " + nextWaypoint.geoLoca + ". Distance = " + GeoAssist.calcDistance(currentLocation, nextWaypoint) * 1000 + "m");
-    	double angle = GeoAssist.calcAngle(currentLocation, nextWaypoint);
-    	double newLat = currentLocation.latitude + Math.sin(angle) * speed;
-    	double newLong = currentLocation.longitude + Math.cos(angle) * speed;
-    	currentLocation = new Location("currentLocation", newLat, newLong);
+	public LidarDrive(String name, double xCoord, double yCoord) {
+        this.name = name;
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+        destination = new Route(new Location(name, xCoord, yCoord));
+    }
+    
+
+    protected void initialize(){
+        initGyro = Math.toRadians(RobotMap.headingGyro.getAngle());
+
+        
+    }
+    protected void execute() {
     }
 
-    
+    protected boolean isFinished() {
+        return false;
+    }
+
+    protected void end() {
+        super.end();
+    }
 }
